fcc125d40ece55cc493f72aeb8559a5cbca522c5
shelliaronov <shelliaronov24@gmail.com>
2026-01-07 20:13:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import numpy as np
+from skimage.filters import median
+from skimage.morphology import ball
+from PIL import Image
+import image_utils
 
+filename = 'IMG_1050 2.jpg'
+
+image = image_utils.load_image(filename)
+
+clean_image = median(image, ball(3))
+
+edgeMAG = image_utils.edge_detection(clean_image)
+
+threshold = 30
+edge_binary = edgeMAG > threshold
+
+edge_image = Image.fromarray((edge_binary * 255).astype(np.uint8))
+edge_image.save('my_edges.png')
